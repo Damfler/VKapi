09603f1ff33d453ff80d7a53ed955265e8932e24
Dima Gashuk <72968420+Damfler@users.noreply.github.com>
2021-09-14 17:43:24 +0300

    update

--- FreindsGet.php
+++ FreindsGet.php
@@ -1,5 +1,7 @@
 <?
+include_once('class.php');
+
 $token = 'd06d18cd0e9deb770ff85d427653564578a9961ea4b4bb6bf5d3c2abb0bf9a90e2d6545a06c5264ff5695';
 $id = '178091703';
 
-file_get_contents('https://api.vk.com/method/friends.get?user_ids=178091703&access_token=c04527264360e2e748d36580540d004e0f4376f033f4d4b66292e4596c22f8ff180ad1cf72eb14602db04&v=5.131')
+$list = getFriends($id, $token);
