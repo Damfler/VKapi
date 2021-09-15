15d28598db5cb7bb24b16ba4db8d74178479227e
Dima Gashuk <72968420+Damfler@users.noreply.github.com>
2021-09-15 09:02:27 +0300

    refactor + add func

--- class.php
+++ class.php
@@ -1,13 +1,19 @@
 <?
 
-function getFriends ($id, $token) {
+function getFriends ($id, $token) { /* Получение всех друзей */
 
-    return file_get_contents("https://api.vk.com/method/friends.get?user_id=$id&access_token=$token&v=5.131");
+    return file_get_contents("https://api.vk.com/method/friends.get?access_token=$token&v=5.131");
 
 }
 
-function deleteFriends ($id, $token) {
+function deleteFriends ($id, $token) { /* Удаление друзей по id */
 
     return file_get_contents("https://api.vk.com/method/friends.delete?user_id=$id&access_token=$token&v=5.131");
+
+}
+
+function getFollowers ($id, $token) { /* Получение всех подписчиков */
+
+    return file_get_contents("https://api.vk.com/method/users.getFollowers?access_token=$token&v=5.131");
 
 }
