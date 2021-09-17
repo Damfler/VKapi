2caba5503e4e3fed63d36a602c9f5152fe1c99c7
Dima Gashuk <72968420+Damfler@users.noreply.github.com>
2021-09-17 12:03:18 +0300

    refactor

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?
-include_once('user_data.php'); /* Ваши данные */
 include_once('class.php');
 
-$token = 'd06d18cd0e9deb770ff85d427653564578a9961ea4b4bb6bf5d3c2abb0bf9a90e2d6545a06c5264ff5695';
-$id = '178091703';
+$token = '';
+$id = '';
 
 // Get Freinds
 $list = getFriends($id, $token);
